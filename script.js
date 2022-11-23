b50cec20316a120a4875ff42d9a9f822949da4e8
Nuseyba <n.kenadiid@gmail.com>
2022-11-23 18:35:44 +0000

    adjusted text area

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentTime);
+
+
+
+var inputAnswer = document.getElementById("#answer")
+var buttonEl = document.getElementById("button")
+
+buttonEl.addEventListener("click", saveToStorage())
+
+function saveToStorage() {
+localStorage.setItem("task", inputAnswer.innerHTML);
+}
